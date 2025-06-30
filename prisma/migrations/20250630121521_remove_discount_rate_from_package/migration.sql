210f5a509543422eca2857c92bc8350f98a7c4f0
yooseungyeop <yooseungyub@naver.com>
2025-06-30 21:15:46 +0900

    fix/remove-package-discountRateField

---
+++ prisma/migrations/20250630121521_remove_discount_rate_from_package/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `discount_rate` on the `packages` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "packages" DROP COLUMN "discount_rate";
